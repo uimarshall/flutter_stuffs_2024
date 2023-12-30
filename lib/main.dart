e3212c2d23336841c1dfae321b5a74d9078abe32
uimarshall <uimarshall@gmail.com>
2023-12-30 21:45:40 +0000

    Add containers and rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/home_page.dart';
+import 'package:flutter_stuffs_2024/screens/loading_page.dart';
+// import 'screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: HomePage(),
+      home: LoadingPage(),
     );
   }
 }
