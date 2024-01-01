fd839875fa64aa45fa182cfaed45595f64f28c97
uimarshall <uimarshall@gmail.com>
2024-01-01 12:41:19 +0000

    Add custom classes and properties

---
+++ lib/lists/menu_item.dart
@@ -0,0 +1,13 @@
+class MenuItem {
+  String title;
+  String reviews;
+  MenuItem({required this.title, required this.reviews});
+  // Use named parameters to make the code more readable
+
+  // MenuItem({String title, String reviews}) {
+  //   this.title = title;
+  //   this.reviews = reviews;
+  // }
+}
+
+// MenuItem menu = MenuItem(title: 'Breakfast', reviews: 'Tastes good');
