e11d6547a14d2c9b1ed45c6230f8fa1643fdb894
uimarshall <uimarshall@gmail.com>
2024-01-20 15:31:31 +0000

    Add clip path to home screen

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -6,13 +6,13 @@ import 'package:flutter_stuffs_2024/shared/widgets/custom_shapes/curved_edges/cu
 import 'package:flutter_stuffs_2024/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SingleChildScrollView(
-        // We col because the elements will be aligned on the from top to bottom and scrollable
+        // We use col because the elements will be aligned on the from top to bottom and scrollable
         child: Column(
           children: [
             // Add custom shape to the container by wrapping it with a ClipPath Widget.
